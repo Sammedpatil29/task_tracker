cc5296107fc536e60a6ab1fb5583c6cbac2fb246
Sammed <sammed.patil29@gmail.com>
2026-09-09 22:39:13 +0530

    prod bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://discipline-tracker-backend-xckgge-ddd24d-203-57-85-153.sslip.io/'
+  apiUrl: 'https://discipline-tracker-backend-xckgge-ddd24d-203-57-85-153.sslip.io/'
 };
 
